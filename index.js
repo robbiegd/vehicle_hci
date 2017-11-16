fa32d7f1a26a3cbfd3901a59d7f4ffb044587f4d
Eduardo Ornelas <eddie@Eduardos-MacBook-Pro.local>
2017-11-15 20:40:37 -0700

    Pushing up the right files... my bad

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
-// get a reference to an element
 var stage = document.getElementById('stage');
 $stage = jQuery(stage);
 
 // create a manager for that element
 var manager = new Hammer.Manager(stage);
 
-// create recognizers
-var Pan = new Hammer.Pan();
-var Rotate = new Hammer.Rotate();
-var Pinch = new Hammer.Pinch();
-var Tap = new Hammer.Tap({
-  taps: 1
+var Pan = new Hammer.Pan({
+    pointers: 2,
 });
-var DoubleTap = new Hammer.Tap({
-  event: 'doubletap',
-  taps: 2
+var Swipe = new Hammer.Swipe({
+    pointers: 2,
 });
-var Swipe = new Hammer.Swipe();
-
-// use them together
-Rotate.recognizeWith([Pan]);
-Pinch.recognizeWith([Rotate, Pan]);
 
 Swipe.recognizeWith([Pan])
 
-DoubleTap.recognizeWith([Tap]);
-Tap.requireFailure([DoubleTap]);
-
-// add the recognizers
 manager.add(Pan);
-manager.add(Rotate);
-manager.add(Pinch);
-manager.add(DoubleTap);
-manager.add(Tap);
-manager.add(Swipe);
-
-// subscribe to events
-var liveScale = 1;
-var currentRotation = 0;
-manager.on('rotatemove', function(e) {
-    // do something cool
-    alert(e);
-    var rotation = currentRotation + Math.round(liveScale * e.rotation);
-    $.Velocity.hook($stage, 'rotateZ', rotation + 'deg');
-});
-manager.on('rotateend', function(e) {
-    // cache the rotation
-    currentRotation += Math.round(e.rotation);
-});
+manager.add(Swipe)
 
 manager.on('swipe', function (e){
     if(e.direction === 2)
@@ -56,71 +23,92 @@ manager.on('swipe', function (e){
         alert("Swipe Right")
 });
 
-// var deltaX = 0;
-// var deltaY = 0;
-// manager.on('panmove', function(e) {
-//   // do something cool
-//   var dX = deltaX + (e.deltaX);
-//   var dY = deltaY + (e.deltaY);
-//   $.Velocity.hook($stage, 'translateX', dX + 'px');
-//   $.Velocity.hook($stage, 'translateY', dY + 'px');
-// });
-// manager.on('panend', function(e) {
-//   deltaX = deltaX + e.deltaX;
-//   deltaY = deltaY + e.deltaY;
-// });
-
-// subscribe to events
-var currentScale = 1;
-function getRelativeScale(scale) {
-  return scale * currentScale;
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: {lat: 32.2319, lng: -110.9501},
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ],
+          disableDefaultUI: true,
+    });
 }
-manager.on('pinchmove', function(e) {
-  // do something cool
-  var scale = getRelativeScale(e.scale);
-  $.Velocity.hook($stage, 'scale', scale);
-});
-manager.on('pinchend', function(e) {
-  // cache the scale
-  currentScale = getRelativeScale(e.scale);
-  liveScale = currentScale;
-});
 
-var colors = [
-  [20, 187, 95],
-  [20, 95, 187],
-  [187, 95, 20],
-  [187, 20, 95],
-  [95, 20, 187],
-  [95, 187, 20]
-];
-function mult(a, b) {
-  return Math.round(a * b);
-}
-function makeColor(rgb, adj) {
-  adj = adj || 1;
-  return 'rgb('+mult(rgb[0], adj)+','+mult(rgb[1], adj)+','+ mult(rgb[2], adj)+')';
-}
-var currentColorIndex = 0;
-manager.on('tap', function(e) {
-  currentColorIndex++;
-  if (currentColorIndex >= colors.length) {
-    currentColorIndex = 0;
-  }
-  stage.style.backgroundColor = makeColor(colors[currentColorIndex]);
-  stage.style.borderColor = makeColor(colors[currentColorIndex], 0.85);
-});
-
-var isShrunken = false;
-manager.on('doubletap', function() {
-  console.log('doubletapped');
-  var scale = $.Velocity.hook($stage, 'scale');
-  if (isShrunken) {
-    $.Velocity.hook($stage, 'scale', 2 * scale);
-  } else {
-    $.Velocity.hook($stage, 'scale', .5 * scale);
-  }
-  isShrunken = !isShrunken;
-});
-
-
+initMap();
